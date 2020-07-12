559ce61421272423e91efcdfd92323e9d00916a5
rbgeorgy <invariantcalibration@gmail.com>
2020-07-12 20:04:10 +0300

    webm implementation

---
+++ bash/webm/webm.sh
@@ -0,0 +1,11 @@
+curl -s $1 | grep -o "image_url=https://2ch.hk/b/src/[0-9]*/[0-9]*.\(webm\|mp4\)" | sed "s/image_url=//" > videos_from_dvach.txt
+mkdir downloads
+for temp in $(cat videos_from_dvach.txt)
+do 
+    cd downloads
+    temp2=$(echo $temp | sed "s/https\:\/\/2ch\.hk\/b\/src\/[0-9]*\///")
+    curl $temp -o $temp2
+    cd ..
+done
+rm videos_from_dvach.txt
+audacious downloads
