4e43f3e36eb61544699c05fd74d296ccd9573ad0
rbgeorgy <invariantcalibration@gmail.com>
2020-07-13 06:07:43 +0300

    .

--- bash/jpg/jpg.sh
+++ bash/jpg/jpg.sh
@@ -1,12 +1,13 @@
 tmpforgrep=$(echo $1 | sed "s/res\/[0-9]*\.html$//")
 #echo $tmpforgrep
 curl -s $1 | grep -o "image_url=${tmpforgrep}src/[0-9]*/[0-9]*.jpg" | sed "s/image\_url\=//" > t.txt
-mkdir download
 boardname=$(echo $tmpforgrep | sed "s/https\:\/\/2ch\.hk\///" | sed "s/\/$//")
+dirname=$(echo $1 | sed "s/https\:\/\/2ch\.hk\/wp\/res\///" | sed "s/\.html$//")
+mkdir $dirname
 #echo $boardname
 for file in $(cat t.txt)
 do
-    cd download
+    cd $dirname
     file1=$(echo $file | sed "s/^https\:\/\/2ch\.hk\/${boardname}\/src\/[0-9]*\///")
     curl -s $file -o $file1
     cd ..
